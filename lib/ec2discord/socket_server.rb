f8118b967693dd36bc4469bfee6695accffa0c7a
highemerly <highemerly@me.com>
2021-01-30 23:16:47 +0900

    [Add] rescue rule for socket server

--- lib/ec2discord/socket_server.rb
+++ lib/ec2discord/socket_server.rb
@@ -13,17 +13,23 @@ module Ec2discord
 
     def run
       loop do
-        client = @server.accept
-        res = client.recv(2048)
-        client.puts "RECEIVE"
+        begin
+          client = @server.accept
+          res = client.recv(2048)
+          client.puts "RECEIVE"
 
-        if res.start_with?("public_ipv4:") then
-          @ipv4_addr = res.gsub(/public_ipv4:/,'').strip
+          if res.start_with?("public_ipv4:") then
+            @ipv4_addr = res.gsub(/public_ipv4:/,'').strip
+          end
+          p "[Socket Server]: Received EC2 public ipv4 address, #{@ipv4_addr}"
+          $log.info("[Socket Server]: Recieved EC2 public ipv4 address, #{ipv4_addr}")
+
+          @dns.update(@ipv4_addr)
+
+          client.close
+        rescue => e
+          $log.error("[Socket Server]: At #{e.class}, #{e.message}.")
         end
-        p "[Socket Server]: Received EC2 public ipv4 address, #{@ipv4_addr}"
-        @dns.update(@ipv4_addr)
-
-        client.close
       end
     end
 
